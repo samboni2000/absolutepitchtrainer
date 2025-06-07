85cf2b017ee158552c68e8e09fddb4610eb4b3be
samboni2000 <80.worktop.morel@icloud.com>
2025-06-07 16:11:04 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-
-const CACHE='shepard-v5';
-const ASSETS=['./','index.html','manifest.json','icons/192.png','icons/512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='shep-v6';const ASSETS=['./','index.html','manifest.json','icons/192.png','icons/512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
